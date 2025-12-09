d73125b0800c21c271d55e71728829ec851a430f
Sarthak <applespro75@gmail.com>
2025-12-09 09:24:01 +0530

    fixes

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.cf2f9f62vgk"
+    "revision": "0.jh816rmfvmo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
